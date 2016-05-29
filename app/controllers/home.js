dbd32cc993cdacc3d4875d223419da44ef170d1b
Federico Gallucci <federico.gallucci@spindox.it>
2016-05-29 04:03:58 +0200

    trying to move motors!

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -2,9 +2,24 @@
  * Created by fgallucci on 29/05/16.
  */
 
+var gpio = require("pi-gpio");
+
 var router = module.exports = require('express').Router();
 
 router.get("/",function(req,res){
   log.info("RASPBOOOOMMM  HOME");
   res.send("RASSPPBOOOOMMMMMMM!!!")
 });
+
+router.post("/move",function(req,res){
+  gpio.write(16, 1, function() {
+    sleep(1000);
+    gpio.write(16, 0, function() {
+      sleep(1000);
+      if(!stopMotors)move();
+    });
+  });
+});
+
+
+
