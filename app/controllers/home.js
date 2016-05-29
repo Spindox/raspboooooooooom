56ca44faf0f191ad8d8e6e3488e43476fa5e1c4b
Federico Gallucci <federico.gallucci@spindox.it>
2016-05-29 04:37:17 +0200

    trying to move motors!

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -6,35 +6,27 @@ var gpio = require("pi-gpio");
 
 var router = module.exports = require('express').Router();
 
-router.get("/",function(req,res){
+router.get("/", function (req, res) {
   log.info("RASPBOOOOMMM  HOME");
   res.send("RASSPPBOOOOMMMMMMM!!!")
 });
 
-router.post("/move",function(req,res){
-  gpio.open(0,"output",function(err){
-    if(err)console.log(err);
-    gpio.open(1,"output",function(err){
-      if(err)console.log(err);
-      gpio.open(2,"output",function(err){
-        if(err)console.log(err);
+router.post("/move", function (req, res) {
+  gpio.open(16, "output", function (err) {
+    if (err)console.log(err);
+    gpio.write(16, 1, function () {
+      sleep(1000);
+      gpio.write(16, 0, function () {
+        sleep(1000);
+        //if (!stopMotors)move();
       });
     });
 
   });
-  gpio.write(0, function() {
-    console.log("0");
-    gpio.write(1, function() {
-      console.log("1");
-      gpio.write(2, function() {
-        console.log("2");
-      });
 
-    });
-  });
 });
 
-router.post("/stop",function(req,res){
+router.post("/stop", function (req, res) {
 
 });
 
